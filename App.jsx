69a45811c74ad2af925816cf7e5bd34ec8d54e0b
Ishashuklaa <146952923+Ishashuklaa@users.noreply.github.com>
2024-04-04 13:25:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import Lottery from './Lottery'
+import './App.css'
+import TicketNum from './TicketNum'
+import Ticket from './Ticket'
+function App() {
+  
+
+  return (
+    <>
+     <Ticket ticket={[0,1,2]}/>
+    </>
+  );
+}
+
+export default App
